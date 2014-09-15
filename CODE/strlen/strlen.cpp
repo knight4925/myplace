9cb6f60c1e4179326800614125551f1acb6f24fd
liuhuimin <liuhuiminlanbo@gmail.com>
2014-09-15 12:58:42 +0800

    长度

--- CODE/strlen/strlen.cpp
+++ CODE/strlen/strlen.cpp
@@ -15,14 +15,15 @@ void test(char c[10]){
 int main()
 {
 
-//	char *s = "abcdefg";
-// 	char c[10]={'a','b','c','d','e','f'};
-// 	cout<<strlen(s)<<endl;
-// 	cout<<sizeof(s)<<endl;
-	char c[10];
-	test(c);
-//	cout<<sizeof(c)<<endl;
-// 	cout<<strlen(c)<<endl;
+	char *s = "abcdefg";
+ 	char c[10]={'a','b','c','d','e','f'};
+ 	cout<<strlen(s)<<endl;
+ 	cout<<sizeof(s)<<endl;
+//	char c[10];
+//	test(c);
+	cout<<strlen(c)<<endl;
+	cout<<sizeof(c)<<endl;
+ 	
 
 //	cout<<mystrlen(s)<<endl;
 
